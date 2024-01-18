a811994cd53493ee1fa86f05e9d37d6d7396b8b0
bitwiserob <kaczurr@gmail.com>
2024-01-18 11:26:57 -0500

    garbage

---
+++ config.py
@@ -0,0 +1,7 @@
+api_keys = dict(
+    {    'alphavantage':'5Z7TGA8JQR8XRYI2',
+         'polygon':'uUlR8L9Vf1ppcPhg15Vpu7rVUfUBY3gY',
+         'nasdaq':'syQboi7vELw-bSvPMJ35',
+         'finnhub':'cmk77chr01qi6gquoavgcmk77chr01qi6gquob00'
+    }
+   )
---
+++ data-engine.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 17 11:03:19 2024
+
+@author: Robert
+
+
+
+"""
+
+
+
+[
+ 'NVDA',
+ 'AAPL',
+ 'ABNB',
+ 'MSFT',
+ 'AMD',
+ 'META',
+]
+
+
+
