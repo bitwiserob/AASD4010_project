9aee96552329ab120f1175bff9d56a719bb26844
bitwiserob <kaczurr@gmail.com>
2024-01-26 16:57:04 -0500

    fixed import

--- regression/baseline_single_ticker_regression.py
+++ regression/baseline_single_ticker_regression.py
@@ -1,4 +1,4 @@
-from preprocess.single_preprocessing_function import single_preprocessing
+from regression.single_preprocessing_function import single_preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_percentage_error
